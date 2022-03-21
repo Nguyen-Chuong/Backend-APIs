5115c9a18252f8a1ee89a4a084a2f2e22135ec85
Nguyen Thuc Nguyen Chuong <63794497+ntnc-se@users.noreply.github.com>
2022-03-21 19:36:09 +0700

    optimize import

--- src/main/java/com/capstone_project/hbts/service/impl/BookingServiceImpl.java
+++ src/main/java/com/capstone_project/hbts/service/impl/BookingServiceImpl.java
@@ -20,8 +20,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
