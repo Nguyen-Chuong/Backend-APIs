4e17791d9f6d23dc776f5bab714b404297f0b461
Nguyen Thuc Nguyen Chuong <63794497+ntnc-se@users.noreply.github.com>
2022-03-22 21:43:56 +0700

    add auditor aware

---
+++ src/main/java/com/capstone_project/hbts/audit/EntityAuditorAware.java
@@ -0,0 +1,24 @@
+package com.capstone_project.hbts.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public class EntityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // if authentication from context is null or unauthenticated
+        if(authentication == null || !authentication.isAuthenticated()){
+            return Optional.empty();
+        }else {
+            // return username of authentication using jwt
+            return Optional.of(((User)authentication.getPrincipal()).getUsername());
+        }
+    }
+
+}
